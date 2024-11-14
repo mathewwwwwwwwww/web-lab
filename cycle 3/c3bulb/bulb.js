d659237943936a9f1217d351d50d0c95bfc93b9b
mathewwwwwwwwww <24mp2317@rit.ac.in>
2024-11-14 10:08:00 +0530

    Add files via upload

---
+++ cycle 3/c3bulb/bulb.js
@@ -0,0 +1,6 @@
+document.getElementById('onButton').addEventListener('click', function() {
+    document.getElementById('lightBulb').src ='bo.jpeg'; 
+});
+document.getElementById('offButton').addEventListener('click', function() {
+    document.getElementById('lightBulb').src ='bulb.jpeg'; 
+});
